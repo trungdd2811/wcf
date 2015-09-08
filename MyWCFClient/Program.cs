d5be39eb7e65b4432869b1ddedf3edaff22812be
trungdd <chat20003qn@gmail.com>
2015-09-08 15:29:49 +0700

    add autofac example

--- MyWCFClient/Program.cs
+++ MyWCFClient/Program.cs
@@ -1,18 +1,36 @@
 ﻿using System;
 using MyWCFClient.demoMathsService;
 using System.ServiceModel;
+using Autofac;
+using DemoAutofac;
 namespace MyWCFClient
 {
     class Program
     {
+        private static IContainer Container { get; set; }
+
+        private static void WriteDate()
+        {
+            // Create the scope, resolve your IDateWriter,
+            // use it, then dispose of the scope.
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var writer = scope.Resolve<IDateWriter>();
+                writer.WriteDate();
+            }
+        }
+
         static void Main(string[] args)
         {
+
+            #region dynamic endpoid
             //dynamic endpoint, url
             //MathsServiceClient obj = new MathsServiceClient("dynamic_url", "http://localhost:54168/Service.svc");
             //MyWCFClient.demoMathsService.Math num = new MyWCFClient.demoMathsService.Math();
             //num.Number1 = 1; num.Number2 = 2;
             //Console.WriteLine(obj.Add(num).ToString());
             //Console.ReadLine();
+            #endregion
 
             #region ChannelFactory example
             //var factory = new ChannelFactory<MyWCF.Contract.IMathsService>("channelFactory");
@@ -25,13 +43,27 @@ namespace MyWCFClient
             #endregion
 
             #region 2-ways binding, MEP stands for Message Exchange Pattern
-            InstanceContext instance = new InstanceContext(new CallBackHandler());
-            
-            var factory = new DuplexChannelFactory<MyWCF.Contract.IDuplexMEPService>(instance, "wsDualBinding");
-            var wcfClientChannel = factory.CreateChannel();
-            Console.WriteLine("call process wcf");      
-            wcfClientChannel.CallProcess();
-            Console.WriteLine(wcfClientChannel.HelloProcess("fdsf"));
+            //InstanceContext instance = new InstanceContext(new CallBackHandler());
+
+            //var factory = new DuplexChannelFactory<MyWCF.Contract.IDuplexMEPService>(instance, "wsDualBinding");
+            //var wcfClientChannel = factory.CreateChannel();
+            //Console.WriteLine("call process wcf");      
+            //wcfClientChannel.CallProcess();
+            //Console.WriteLine(wcfClientChannel.HelloProcess("fdsf"));
+            //Console.ReadLine();
+            #endregion
+
+            #region autofac example
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleOutput>().As<IOutput>();
+            builder.RegisterType<TodayWriter>().As<IDateWriter>();
+            Container = builder.Build();
+
+            // The WriteDate method is where we'll make use
+            // of our dependency injection. We'll define that
+            // in a bit.
+            WriteDate();
+
             Console.ReadLine();
             #endregion
         }
@@ -43,4 +75,6 @@ namespace MyWCFClient
             Console.WriteLine(value);            
         }
     }
+
+
 }
